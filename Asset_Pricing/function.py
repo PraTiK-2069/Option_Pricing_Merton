bc079db8b0a7c110606b1bbcfbe3413438945ec9
PraTiK-2069 <99842045+PraTiK-2069@users.noreply.github.com>
2023-05-05 23:17:03 +0400

    Add files via upload

---
+++ Asset_Pricing/function.py
@@ -0,0 +1,181 @@
+import math
+import pandas as pd
+import numpy as np
+import numpy
+import random
+import decimal
+import scipy.linalg
+import numpy.random as nrand
+import matplotlib.pyplot as plt
+
+
+class ModelParameters:
+    def __init__(self,
+                 all_s0, all_time, all_delta, all_sigma, gbm_mu,
+                 jumps_lamda=0.0, jumps_sigma=0.0, jumps_mu=0.0, all_r0 = 0):
+        # This is the starting asset value
+        self.all_s0 = all_s0
+        # How long you want to stimulate
+        self.all_time = all_time
+        # This is the delta, the rate of time e.g. 1/252 = daily, 1/12 = monthly
+        self.all_delta = all_delta
+        # This is the volatility of the stochastic processes
+        self.all_sigma = all_sigma
+        # This is the annual drift factor for geometric brownian motion
+        self.gbm_mu = gbm_mu
+        # This is the probability of a jump happening at each point in time
+        self.lamda = jumps_lamda
+        # This is the volatility of the jump size
+        self.jumps_sigma = jumps_sigma
+        # This is the average jump size
+        self.jumps_mu = jumps_mu
+        # This is the starting interest rate value
+        self.all_r0 = all_r0
+def jump_diffusion_process(param):
+    """
+    This method produces a sequence of Jump Sizes which represent a jump diffusion process. These jumps are combined
+    with a geometric brownian motion (log returns) to produce the Merton model.
+    :param param: the model parameters object
+    :return: jump sizes for each point in time (mostly zeroes if jumps are infrequent)
+    """
+
+    s_n = time = 0
+    small_lamda = -(1.0 / param.lamda)
+    jump_sizes = []
+    for k in range(0, param.all_time):
+        jump_sizes.append(0.0)
+    while s_n < param.all_time:
+        s_n += small_lamda * math.log(random.uniform(0, 1))
+        for j in range(0, param.all_time):
+            if time * param.all_delta <= s_n * param.all_delta <= (j + 1) * param.all_delta:
+                # print("was true")
+                jump_sizes[j] += random.normalvariate(param.jumps_mu, param.jumps_sigma)
+                break
+        time += 1
+    return jump_sizes
+
+def geometric_brownian_motion_jump_diffusion_log_returns(param):
+    """
+    This method constructs combines a geometric brownian motion process (log returns) with a jump diffusion process
+    (log returns) to produce a sequence of gbm jump returns.
+    :param param: model parameters object
+    :return: returns a GBM process with jumps in it
+    """
+
+    jump_diffusion = jump_diffusion_process(param)
+    geometric_brownian_motion = geometric_brownian_motion_log_returns(param)
+    return numpy.add(jump_diffusion, geometric_brownian_motion)
+
+
+def geometric_brownian_motion_jump_diffusion_levels(param):
+    """
+    This method converts a sequence of gbm jmp returns into a price sequence which evolves according to a geometric
+    brownian motion but can contain jumps at any point in time.
+    :param param: model parameters object
+    :return: the price levels
+    """
+    return convert_to_prices(param, geometric_brownian_motion_jump_diffusion_log_returns(param))
+
+def geometric_brownian_motion_log_returns(param):
+    """
+    This method constructs a sequence of log returns which, when exponentiated, produce a random Geometric Brownian
+    Motion (GBM). GBM is the stochastic process underlying the Black Scholes options pricing formula.
+    :param param: model parameters object
+    :return: returns the log returns of a geometric brownian motion process
+    """
+
+    wiener_process = numpy.array(brownian_motion_log_returns(param))
+    sigma_pow_mu_delta = (param.gbm_mu - 0.5 * math.pow(param.all_sigma, 2.0)) * param.all_delta
+    return wiener_process + sigma_pow_mu_delta
+
+def geometric_brownian_motion_levels(param):
+    """
+    Returns a sequence of price levels for an asset which evolves according to a geometric brownian motion
+    :param param: model parameters object
+    :return: the price levels for the asset
+    """
+    return convert_to_prices(param, geometric_brownian_motion_log_returns(param))
+
+
+def brownian_motion_log_returns(param):
+    sqrt_delta_sigma = math.sqrt(param.all_delta) * param.all_sigma
+    return nrand.normal(loc=0, scale=sqrt_delta_sigma, size=param.all_time)
+
+def brownian_motion_levels(param):
+    """
+    Returns a price sequence whose returns evolve according to a brownian motion
+    :param param: model parameters object
+    :return: returns a price sequence which follows a brownian motion
+    """
+    return convert_to_prices(param, brownian_motion_log_returns(param))
+
+
+
+def plot_stochastic_processes(processes, title):
+    """
+    For plotting paths
+    """
+    plt.style.use(['bmh'])
+    fig, ax = plt.subplots(1)
+    fig.set_figheight(7)
+    fig.set_figwidth(15)
+    fig.suptitle(title, fontsize=16)
+    ax.set_xlabel('Time, t')
+    ax.set_ylabel('Simulated Asset Price')
+    x_axis = numpy.arange(0, len(processes[0]), 1)
+    for i in range(len(processes)):
+        plt.plot(x_axis, processes[i])
+    plt.show()
+def convert_to_returns(log_returns):
+    """
+    converts to normal returns from log
+    """
+    return numpy.exp(log_returns)
+
+def convert_to_prices(param, log_returns):
+    """
+    returns series of price from log_returns
+    """
+    returns = convert_to_returns(log_returns)
+    
+    price_sequence = [param.all_s0]
+    for i in range(1, len(returns)):
+        # Add the price at t-1 * return at t
+        price_sequence.append(price_sequence[i - 1] * returns[i - 1])
+    return numpy.array(price_sequence)
+
+
+def plot_stochastic_processes(processes, title):
+    """
+    For plotting paths
+    """
+    plt.style.use(['bmh'])
+    fig, ax = plt.subplots(1)
+    fig.set_figheight(7)
+    fig.set_figwidth(15)
+    fig.suptitle(title, fontsize=16)
+    ax.set_xlabel('Time, t')
+    ax.set_ylabel('Simulated Asset Price')
+    x_axis = numpy.arange(0, len(processes[0]), 1)
+    for i in range(len(processes)):
+        plt.plot(x_axis, processes[i])
+    plt.show()
+def convert_to_returns(log_returns):
+    """
+    converts to normal returns from log
+    """
+    return numpy.exp(log_returns)
+
+def convert_to_prices(param, log_returns):
+    """
+    returns series of price from log_returns
+    """
+    returns = convert_to_returns(log_returns)
+    
+    price_sequence = [param.all_s0]
+    for i in range(1, len(returns)):
+        # Add the price at t-1 * return at t
+        price_sequence.append(price_sequence[i - 1] * returns[i - 1])
+    return numpy.array(price_sequence)
+
+
